c4ba7d8f5e372b36e4f2d671272ee97ecaeb4f30
ya_sergey@yahoo.com <S32069257b>
2021-07-06 16:41:51 +0300

    first commit

--- javascript/javascript-1.js
+++ javascript/javascript-1.js
@@ -1,16 +1,1 @@
-const calculator = func => {
-    const next = (...args) => {
-        return x => {
-            if (!x) {
-                return args.reduce((acc, a) => {
-                    return func.call(func, acc, a)
-                }, 0);
-            }
-            return next(...args, x);
-        };
-    };
-    return next();
-};
 
-const sum = calculator((x, y) => x + y);
-console.log(sum(1)(3)(4)(2)());
